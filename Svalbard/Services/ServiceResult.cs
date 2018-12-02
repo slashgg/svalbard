dc366b31c64040516b912903a2d6600156c0f203
Martin Lang <martin@mercator.io>
2018-12-01 18:51:24 -0800

    add service result

---
+++ Svalbard/Services/ServiceResult.cs
@@ -0,0 +1,36 @@
+namespace Svalbard.Services
+{
+  public class ServiceResult<T> : ServiceResult
+  {
+    public T Data { get; set; }
+
+    public ServiceResult() { }
+
+    public ServiceResult(bool success) : base(success) { }
+    public ServiceResult(string errorKey) : base(errorKey) { }
+
+    public ServiceResult(T data)
+    {
+      this.Data = data;
+    }
+  }
+
+  public class ServiceResult
+  {
+    public string ErrorKey { get; set; }
+    public bool Success { get; set; } = false;
+
+    public ServiceResult() { }
+
+    public ServiceResult(bool success)
+    {
+      this.Success = success;
+    }
+
+    public ServiceResult(string errorKey)
+    {
+      this.ErrorKey = errorKey;
+      this.Success = false;
+    }
+  }
+}
